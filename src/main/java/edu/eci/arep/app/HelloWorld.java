0a3c4b68d85fc4f7736f00e6ba13576654915403
Juan Pablo <juanpasan2000@gmail.com>
2023-03-17 15:28:06 -0500

    Progress in class

---
+++ src/main/java/edu/eci/arep/app/HelloWorld.java
@@ -0,0 +1,37 @@
+package edu.eci.arep.app;
+
+import static spark.Spark.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // API: secure(keystoreFilePath, keystorePassword, truststoreFilePath,truststorePassword);
+        secure(getKeyStore(), getPwdStore(), null, null);
+
+        port(getPort());
+
+        get("/hello", (req, res) -> "Hello World");
+        // open: http://localhost:5000/hello
+        // keytool -genkeypair -alias ecikeypair -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore ecikeystore.p12 -validity 3650
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 5000; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    static String getKeyStore() {
+        if (System.getenv("KEYSTORE") != null) {
+            return System.getenv("KEYSTORE");
+        }
+        return "certificados/ecikeystore.p12";
+    }
+
+    static String getPwdStore() {
+        if (System.getenv("KEYSTOREPWD") != null) {
+            return System.getenv("KEYSTOREPWD");
+        }
+        return "123456";
+    }
+}
